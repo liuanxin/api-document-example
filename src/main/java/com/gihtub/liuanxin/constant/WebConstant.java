c3b539a7ce9d5945f559ece84cc33f41c9242f9e
liujie <liujie@wolianw.com>
2018-02-11 14:46:48 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/constant/WebConstant.java
+++ src/main/java/com/gihtub/liuanxin/constant/WebConstant.java
@@ -8,10 +8,10 @@ public final class WebConstant {
     public static final String PROJECT_COPYRIGHT = "GPL V3";
 
     public static final String USER_DESC = "user-用户模块";
-    public static final String USER_DEVELOP1 = "xxx - abc@xyz.com";
-    public static final String USER_DEVELOP2 = "xxx - abc@xyz.com";
+    public static final String USER_DEVELOP1 = "xxx - xxx@xyz.com";
+    public static final String USER_DEVELOP2 = "yyy - yyy@xyz.com";
 
     public static final String PRODUCT_DESC = "product-商品模块";
-    public static final String PRODUCT_DEVELOP1 = "yyy - 123@xyz.com";
-    public static final String PRODUCT_DEVELOP2 = "yyy - 123@xyz.com";
+    public static final String PRODUCT_DEVELOP1 = "ppp - ppp@xyz.com";
+    public static final String PRODUCT_DEVELOP2 = "ooo - ooo@xyz.com";
 }
