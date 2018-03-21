36d2a3c227d1b25e76395934d111b499c600c371
liujie <liujie@wolianw.com>
2018-03-21 17:24:07 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/config/ApiInfoConfig.java
+++ src/main/java/com/gihtub/liuanxin/config/ApiInfoConfig.java
@@ -1,11 +1,18 @@
 package com.gihtub.liuanxin.config;
 
 import com.gihtub.liuanxin.constant.WebConstant;
+import com.gihtub.liuanxin.util.JsonCode;
 import com.github.liuanxin.api.annotation.EnableApiInfo;
 import com.github.liuanxin.api.model.DocumentCopyright;
+import com.github.liuanxin.api.model.DocumentResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 @EnableApiInfo
@@ -21,6 +28,22 @@ public class ApiInfoConfig {
                 .setTeam(WebConstant.PROJECT_CONTACT)
                 .setVersion(WebConstant.PROJECT_VERSION)
                 .setCopyright(WebConstant.PROJECT_COPYRIGHT)
-                .setOnline(online);
+                .setOnline(online)
+                .setIgnoreUrlSet(ignoreUrl())
+                .setGlobalResponse(globalResponse());
+    }
+
+    private Set<String> ignoreUrl() {
+        Set<String> urlSet = new HashSet<>();
+        urlSet.add("/error");
+        return urlSet;
+    }
+
+    private List<DocumentResponse> globalResponse() {
+        List<DocumentResponse> responseList = new ArrayList<>();
+        for (JsonCode code : JsonCode.values()) {
+            responseList.add(new DocumentResponse(code.getFlag(), code.getMsg()));
+        }
+        return responseList;
     }
 }
--- src/main/java/com/gihtub/liuanxin/util/JsonResult.java
+++ src/main/java/com/gihtub/liuanxin/util/JsonResult.java
@@ -6,36 +6,62 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
+/** <span style="color:red;">!!!此实体类请只在 Controller 中使用, 且只调用其 static 方法!!!</span> */
 @Setter
+@Getter
 @NoArgsConstructor
 public class JsonResult<T> {
 
-    @ApiReturn(desc = "返回码. 根据此值控制页面扭转: 0.显示 msg, 1.业务处理, 10.导向登录页 等")
+    /**
+     * 返回码. 前台根据此值控制页面扭转.
+     *
+     * @see JsonCode
+     */
+    @ApiReturn(desc = "返回码. 根据此值控制页面扭转: 0.显示 msg, 1.业务处理, 10.导向登录页")
     private int code;
 
+    /** 返回说明. 如: 用户名密码错误, 收货地址添加成功 等 */
     @ApiReturn(desc = "返回说明. 如: 用户名密码错误, 收货地址添加成功 等")
     private String msg;
 
-    @ApiReturn(desc = "返回的数据. 返回实体 {\"id\":1} 还是列表 [{\"id\":1},{\"id\":2}] 依具体的业务而定")
+    /** 返回的数据. 具体是返回实体 {"id":1} 还是列表 [{"id":1},{"id":2}] 依具体的业务而定 */
+    @ApiReturn(desc = "返回的数据. 实体 {\"id\":1} 还是列表 [{\"id\":1},{\"id\":2}] 依具体的业务而定")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
-    private JsonResult(int code, String msg) {
-        this.code = code;
+    private JsonResult(JsonCode code, String msg) {
+        this.code = code.flag;
         this.msg = msg;
     }
-    private JsonResult(int code, String msg, T data) {
-        this.code = code;
-        this.msg = msg;
+    private JsonResult(JsonCode code, String msg, T data) {
+        this(code, msg);
         this.data = data;
     }
 
 
-    public static <T> JsonResult<T> fail(String msg) {
-        return new JsonResult<T>(0, msg);
-    }
+    // ---------- 在 service 中请只使用下面的静态方法就好了. 不要 new JsonResult()... 这样操作 ----------
+
+    /** 请求成功且不需要返回数据, 当返回 "地址添加成功" 这一类说明时 */
     public static <T> JsonResult<T> success(String msg) {
-        return new JsonResult<T>(1, msg);
+        return new JsonResult<>(JsonCode.SUCCESS, msg);
+    }
+    /** 请求成功且有返回数据时 */
+    public static <T> JsonResult<T> success(String msg, T data) {
+        return new JsonResult<>(JsonCode.SUCCESS, msg, data);
+    }
+
+    /** 请求失败时 */
+    public static <T> JsonResult<T> fail(String msg) {
+        return new JsonResult<>(JsonCode.FAIL, msg);
+    }
+
+    /** 未登录时 */
+    public static <T> JsonResult<T> notLogin() {
+        return new JsonResult<>(JsonCode.NOT_LOGIN, "Need Login");
+    }
+
+    /** 无权限时 */
+    public static <T> JsonResult<T> notPermission(String msg) {
+        return new JsonResult<>(JsonCode.NOT_PERMISSION, msg);
     }
 }
