63c8b2a6f77bfe4bbd000dbff82873d0be5ead3d
liujie <liujie@yunliketech.com>
2018-07-23 10:48:28 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/constant/Develop.java
+++ src/main/java/com/gihtub/liuanxin/constant/Develop.java
@@ -4,7 +4,7 @@ public class Develop {
 
     // 模块及开发人员
 
-    public static final String USER_DESC = "user";
+    public static final String USER_DESC = "user-用户";
     public static final String PRODUCT_DESC = "product-商品";
 
     public static final String USER = "xxx - xxx@xyz.com";
