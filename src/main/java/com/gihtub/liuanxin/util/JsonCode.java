30b9fb14dd99588ff4c2f1d46532adcefd773769
liujie <liu_anxin@163.com>
2019-03-01 13:59:53 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/util/JsonCode.java
+++ src/main/java/com/gihtub/liuanxin/util/JsonCode.java
@@ -5,15 +5,15 @@ public enum JsonCode {
 
     SUCCESS(200, "成功. 操作数据或显示 msg 给用户看, 依具体的业务而定"),
 
-    BAD_REQUEST(400, "参数有误(输出 msg 即可)"),
+    BAD_REQUEST(400, "参数有误(输出 response body 即可)"),
 
     NOT_LOGIN(401, "未登录, 导到登录页"),
 
-    NOT_PERMISSION(403, "无权限(输出 msg 即可)"),
+    NOT_PERMISSION(403, "无权限(输出 response body 即可)"),
 
-    NOT_FOUND(404, "未找到相应处理(不需要处理)"),
+    NOT_FOUND(404, "未找到相应处理"),
 
-    FAIL(500, "内部错误、业务异常(输出 msg 即可)")
+    FAIL(500, "内部错误、业务异常(输出 response body 即可)")
 
     // , SERVICE_FAIL(1000, "业务异常")
     ;
