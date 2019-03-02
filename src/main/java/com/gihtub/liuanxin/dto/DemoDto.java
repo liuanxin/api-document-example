dd5940e4469987d38cb27cb98aa7fd18d0928c92
liujie <liu_anxin@163.com>
2019-03-02 13:50:36 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/dto/DemoDto.java
+++ src/main/java/com/gihtub/liuanxin/dto/DemoDto.java
@@ -1,6 +1,7 @@
 package com.gihtub.liuanxin.dto;
 
 import com.gihtub.liuanxin.enums.Gender;
+import com.gihtub.liuanxin.exception.ServiceException;
 import com.github.liuanxin.api.annotation.ApiParam;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,12 +14,12 @@ public class DemoDto {
     private Long userId;
 
     // 文档收集 会将 enum 的 getCode: getValue 信息(没有则拼接 name)附加进说明
-    @ApiParam(value = "性别", dataType = "int")
+    @ApiParam(value = "性别", dataType = "ipv4")
     private Gender gender;
 
     public void basicCheck() {
         if (userId == null || userId <= 0) {
-            throw new RuntimeException("用户 id 必须传入且要是数字且必须大于 0");
+            throw new ServiceException("用户 id 必须传入且要是数字且必须大于 0");
         }
     }
 }
