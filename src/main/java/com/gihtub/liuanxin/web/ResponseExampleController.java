07084316e3b21dd395d821dc77dc60201ef84c60
liujie <liu_anxin@163.com>
2019-03-05 17:08:00 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/web/ResponseExampleController.java
+++ src/main/java/com/gihtub/liuanxin/web/ResponseExampleController.java
@@ -7,18 +7,22 @@ import com.gihtub.liuanxin.exception.ServiceException;
 import com.gihtub.liuanxin.util.Page;
 import com.gihtub.liuanxin.vo.DemoVo;
 import com.github.liuanxin.api.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 @RequestMapping("/response")
 @ApiGroup("response-响应")
 public class ResponseExampleController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResponseExampleController.class);
 
     @ApiMethod(title = "响应实体", develop = Develop.PRODUCT, index = 3)
     @ApiResponses({
@@ -29,11 +33,40 @@ public class ResponseExampleController {
     public ResponseEntity<DemoVo> demoObject(@ApiParam(value = "商品名", textarea = true) @RequestParam("name") String abc,
                                              @ApiParam(value = "头1", paramType = ParamType.Header) @RequestHeader Long id,
                                              @ApiParam(value = "头2", paramType = ParamType.Header) @RequestHeader("some") String xyz,
+                                             @ApiParam("文件上传1") MultipartFile file1,
+                                             @ApiParam(value = "文件上传2", must = true) MultipartFile file2,
                                              Page page) {
-
         if ("abc".equals(abc)) {
             throw new ServiceException("商品名有误");
         }
+        if (id == null || id < 0) {
+            throw new ServiceException("头1 要有值且必须大于等于 0");
+        }
+        if (xyz == null || "".equals(xyz.trim())) {
+            throw new ServiceException("头2 不能为空");
+        }
+        if (file2 == null) {
+            throw new ServiceException("文件上传2 不能为空");
+        }
+
+        String fileName = file2.getOriginalFilename();
+        if (fileName == null || !fileName.matches("(?i)^(.*)\\.(ico|jpeg|jpg|bmp|png)$")) {
+            throw new ServiceException("请上传图片文件");
+        }
+
+        // save file
+        try {
+            String saveFile = UUID.randomUUID().toString().replace("-", "") + fileName.substring(fileName.lastIndexOf("."));
+            file2.transferTo(new File("/" + saveFile));
+        } catch (IOException e) {
+            String msg = "保存文件异常";
+            if (LOGGER.isErrorEnabled()) {
+                LOGGER.error(msg, e);
+            }
+            throw new ServiceException(msg);
+        }
+
+        // 正常返回
         return ResponseEntity.ok(new DemoVo(123L, "张三", Gender.Male, ProductType.Discount, null));
     }
 
@@ -62,7 +95,7 @@ public class ResponseExampleController {
     public ResponseEntity<Map<String, DemoVo>> demoMap(@ApiParam(value = "商品名", textarea = true) String name,
                                                        Page page) {
         if ("xyz".equals(name)) {
-            return ResponseEntity.status(401).build();
+            return ResponseEntity.status(403).build();
         } else {
             return ResponseEntity.ok(Collections.singletonMap(
                     "123", new DemoVo(123L, "张三", Gender.Male, ProductType.Discount, null)
