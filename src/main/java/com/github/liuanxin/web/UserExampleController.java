fc76351d4eb63de203db0bf629eb5aa5c521675e
TY <liu.anxin13@gmail.com>
2022-10-22 22:58:10 +0800

    fix

--- src/main/java/com/github/liuanxin/web/UserExampleController.java
+++ src/main/java/com/github/liuanxin/web/UserExampleController.java
@@ -1,5 +1,7 @@
 package com.github.liuanxin.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.liuanxin.api.annotation.*;
 import com.github.liuanxin.constant.Develop;
 import com.github.liuanxin.enums.UserType;
@@ -10,6 +12,8 @@ import com.github.liuanxin.res.DemoRes;
 import com.github.liuanxin.util.JsonResult;
 import com.github.liuanxin.util.Page;
 import com.github.liuanxin.util.PageInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,18 +22,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @ApiGroup(value = Develop.USER_DESC, index = 1)
+@Slf4j
+@RequiredArgsConstructor
 public class UserExampleController {
+
+    private final ObjectMapper objectMapper;
 
     @ApiMethod(value = "用户列表", develop = Develop.USER, index = 1, desc = "管理员查询用户")
     @GetMapping
     public JsonResult<PageInfo<DemoRes>> demo1(DemoReq demoReq, Page page) {
-        return JsonResult.success("test1");
+        return JsonResult.success("test1", DemoRes.testPageData());
     }
 
     @ApiMethod(value = "用户信息", develop = Develop.USER, index = 2, commentInReturnExample = false)
     @GetMapping("/info")
     public JsonResult<PageInfo<DemoRes>> demo2(@ApiParam("用户类型") UserType type) {
-        return JsonResult.success("test2");
+        return JsonResult.success("test2", DemoRes.testPageData());
     }
 
     @ApiMethod(value = "用户详情", develop = Develop.USER, desc = "当用户点击头像时")
@@ -39,7 +47,7 @@ public class UserExampleController {
     })
     @PostMapping("/{id}")
     public JsonResult<DemoRes> demo3(@PathVariable("id") @ApiParam(value = "user id", example = "1") Long id) {
-        return JsonResult.success("test3");
+        return JsonResult.success("test3", DemoRes.testData());
     }
 
 //    @ApiMethod(value = "用户操作", develop = Develop.USER)
@@ -54,13 +62,18 @@ public class UserExampleController {
     @ApiMethod(value = "使用 RequestBody", develop = Develop.USER)
     @PostMapping("/detail")
     public JsonResult<List<DemoRes>> demo5(@RequestBody RequestBodyReq demoDto) {
-        return JsonResult.success("test5");
+        return JsonResult.success("test5", DemoRes.testListData());
     }
 
     @ApiMethod(value = "使用 RequestBody(嵌套)", develop = Develop.USER)
     @PostMapping("/add-info")
     public JsonResult<List<DemoRes>> demo6(@RequestBody RequestBodyInnerReq demoDto) {
-        return JsonResult.success("test6");
+        try {
+            log.info("参数: " + objectMapper.writeValueAsString(demoDto));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return JsonResult.success("test6", DemoRes.testListData());
     }
 
     @ApiIgnore
