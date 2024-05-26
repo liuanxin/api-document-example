1131ac2f855e26dacf202edb3e5fc907510637a1
TY <liu.anxin13@gmail.com>
2024-05-26 22:01:02 +0800

    fix

--- src/main/java/com/github/liuanxin/web/UserExampleController.java
+++ src/main/java/com/github/liuanxin/web/UserExampleController.java
@@ -23,7 +23,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserExampleController {
 
-    @ApiMethod(value = "用户列表", develop = Develop.USER, index = 1, desc = "管理员查询用户")
+    @ApiMethod(value = "用户列表", develop = Develop.USER, index = 1, desc = "管理员查询用户<br><br>" +
+            "比如约定好的 appId 是 aaa, secret 是 xyz, 要传递的参数是 id=123&name=x&gender=1<br>" +
+            "1. 将参数值按自然序排列: gender=1&id=123&name=x<br>" +
+            "2. 生成时间戳(到秒即可, 此处仅示例)及随机数(不带 - 的 32 位 uuid, 此处仅示例): timestamp=11111&nonce=abc123<br>" +
+            "3. 将 排序后的参数 + 时间戳的值 + 随机数的值 + appId 的值 + secret 的值 拼起来: gender=1&id=123&name=x&11111&abc123&aaa&xyz<br>" +
+            "4. 生成签名值: md5(gender=1&id=123&name=x&ms=11111&nonce=abcde&appId=abc&appSecret=xyz) = a241f4ef3a4bc866df88bbdeeb021696<br>" +
+            "<br>最终发送时:<br>" +
+            "请求头(加上 appId 及加密后的值): x-app: abc, x-sign: a241f4ef3a4bc866df88bbdeeb021696<br>" +
+            "参数(在原来的基础上加上时间戳及随机数): id=123&name=x&gender=1 & timestamp=11111&nonce=abc123")
     @GetMapping
     public JsonResult<PageInfo<DemoRes>> demo1(DemoReq demoReq, Page page) {
         return JsonResult.success("test1", DemoRes.testPageData());
